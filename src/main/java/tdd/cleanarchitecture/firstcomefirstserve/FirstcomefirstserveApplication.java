b8af11337c9bc9b21783b504d0ca50eed05cafdf
JO <71416000+insightp25@users.noreply.github.com>
2024-06-24 10:18:37 +0900

    init project

---
+++ src/main/java/tdd/cleanarchitecture/firstcomefirstserve/FirstcomefirstserveApplication.java
@@ -0,0 +1,13 @@
+package tdd.cleanarchitecture.firstcomefirstserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstcomefirstserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstcomefirstserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tdd/cleanarchitecture/firstcomefirstserve/FirstcomefirstserveApplicationTests.java
@@ -0,0 +1,13 @@
+package tdd.cleanarchitecture.firstcomefirstserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstcomefirstserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
